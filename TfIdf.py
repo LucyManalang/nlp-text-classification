de81fd5142d74fde103c451e94c5050f8d90b2ce
LucyManalang <lucemanalang@gmail.com>
2024-11-19 18:28:02 -0600

    Tf-Idf work so far, still not done

--- TfIdf.py
+++ TfIdf.py
@@ -4,41 +4,42 @@ from collections import Counter
 
 class TfIdf:
     def __init__(self, train_data : Iterable[Tuple[Sequence[str], int]]):
+        train_data = [("good boy".split(), 0), ("good girl".split(), 1), ("boy girl good".split(), 2)]
         self.classes = set([c for _, c in train_data])
 
-        self.class_data = {} # key = class, value = sequence of all words in class
-        self.vocab = {} # key = word, value = index
+        # raw data for each class
+        self.class_data = {} # key = class, value = sequence of all data in class
+        # vocab for each class
+        self.class_vocab = {} # key = class, value = set of all words in class
         for c in self.classes:
             data = [t[0] for t in train_data if t[1] == c]
-            self.class_data[c] = [item for sublist in data for item in sublist]
-        self.vocab = {word: i for i, word in enumerate(set(self.class_data[c] for c in self.classes))}
-
-        idf = self.inverse_document_frequency()
-        # self.tfidf_vectors = np.array([self.term_frequency(c) * idf for c in self.classes])
-
-
+            self.class_data[c] = [item for sublist in data for item in sublist] # flatten list
+            self.class_vocab[c] = set(self.class_data[c])
         
+        # set of all words in the vocabulary
+        self.vocab = {word: i for i, word in enumerate(set(word for v in self.class_vocab.values() for word in v))} # key = word, value = index
+
+        self.tf = [self.term_frequency(c) for c in self.classes] # term frequency for each class
+        self.idf = self.inverse_document_frequency() # inverse document frequency for each word
+        self.tfidf = [[(self.tf[c][i] * self.idf[i], c, i) for i in range(len(self.vocab))] for c in self.classes]
+
     def term_frequency(self, c : int) -> Sequence[int]:
         term_freq = np.zeros(len(self.vocab))
-        frequencies = Counter(self.class_data[c])
+        frequencies = Counter(self.class_data[c]) # gets the counts of each word in the class
 
         for word, count in frequencies.items():
             if word in self.vocab:
-                term_freq[self.vocab[word]] = count
-        
+                term_freq[self.vocab[word]] = count / len(self.class_data[c])
+
         return term_freq
     
     def inverse_document_frequency(self) -> Sequence[int]:
         idf = np.zeros(len(self.vocab))
 
         for word in self.vocab:
-            doc_freq = 0
-            for data in self.class_data.values():
-                if word in data:
-                    doc_freq += 1
-            idf[self.vocab[word]] = np.log(len(self.class_data) / (doc_freq + 1))
+            doc_freq = sum(1 for v in self.class_vocab.values() if word in v)
+            idf[self.vocab[word]] = np.log(len(self.class_data) / (1 + doc_freq)) 
         return idf
     
     def label(self, data : Iterable[str]) -> int:
-        print(self.tfidf_vectors)
-        return np.argmax(np.dot(self.tfidf_vectors, np.array([self.term_frequency(self.class_vocab[c], c) for c in self.classes])))
+        return 0
--- main.py
+++ main.py
@@ -38,12 +38,8 @@ elif args.model == "tfidf":
 if args.measure == "acc":
     labeled_data = list(dataset.get_dev_examples())
     true_labels = set([(" ".join(t[0]), t[1]) for t in labeled_data])
-    true_data = set([" ".join(t[0]) for t in labeled_data]) # used to filter out relevant data
-    
-    unlabeled_data = [sentence.split() for sentence in list(dataset.get_test_examples())]
-    relevant_data = [sentence for sentence in unlabeled_data if " ".join(sentence) in true_data] # because dev data does not contain all of test data, filtering out relevant data is necessary
-    predicted_labels = [(" ".join(sentence), model.label(sentence)) for sentence in relevant_data]
-    
-    print("acc: {:.3}".format(100 *accuracy(true_labels, predicted_labels)))
 
-
+    unlabeled_data = [t[0] for t in labeled_data]
+    # predicted_labels = [(" ".join(sentence), model.label(sentence)) for sentence in unlabeled_data]
+    # model.label("this is a postive sentence good amazing loved")
+    # print("acc: {:.3}".format(100 * accuracy(true_labels, predicted_labels)))
